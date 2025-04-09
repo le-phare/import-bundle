4f7e5971da31ca039a05c990799a4e8e0f9960ef
Alexis <mazaudiera@gmail.com>
2025-04-09 11:53:09 +0200

    fix: doctrine.dbal.url must be optional

--- DependencyInjection/Compiler/CredentialsConfigurationPass.php
+++ DependencyInjection/Compiler/CredentialsConfigurationPass.php
@@ -18,6 +18,26 @@ class CredentialsConfigurationPass implements CompilerPassInterface
         // We can "steal" the definition from database_connection definition
         $connectionDefinition = $container->getDefinition('doctrine.dbal.default_connection');
         $credentials = $connectionDefinition->getArgument(0);
-        $definition->replaceArgument('$url', $credentials['url']);
+        $url = $credentials['url'] ?? null;
+        if (!$url && !empty($credentials['host'])) {
+            $options = [];
+            if (!empty($credentials['server_version'])) {
+                $options['server_version'] = $credentials['server_version'];
+            }
+            if (!empty($credentials['charset'])) {
+                $options['charset'] = $credentials['charset'];
+            }
+            $url = sprintf(
+                '%s://%s:%s@%s:%s/%s%s',
+                str_replace('pdo_', '', $credentials['driver']),
+                $credentials['user'],
+                $credentials['password'],
+                $credentials['host'],
+                $credentials['port'],
+                $credentials['dbname'],
+                !empty($options) ? '?'.http_build_query($options) : ''
+            );
+        }
+        $definition->replaceArgument('$url', $url);
     }
 }
