314cf6149a0870b09fbcfcf12ee622dab0f85ab6
Thibaut Selingue <7049020+thislg@users.noreply.github.com>
2025-08-27 16:23:32 +0200

    fix(deprecation): fix deprecated usage of HttpKernel Extension (#37)

--- DependencyInjection/LePhareImportExtension.php
+++ DependencyInjection/LePhareImportExtension.php
@@ -7,8 +7,8 @@ use LePhare\Import\LoadStrategy\LoadStrategyInterface;
 use LePhare\ImportBundle\DependencyInjection\Compiler\ImportLoaderPass;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Mailer\Mailer;
 
 /**
